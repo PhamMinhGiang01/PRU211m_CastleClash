e867e5bf587a8766173dc3daf6a5a4dffdf890e0
thanhpdhe150023@fpt.edu.vn <108501377+thanh23102001@users.noreply.github.com>
2023-03-19 16:25:55 +0700

    update

--- Assets/Srcipts/UI/UIController.cs
+++ Assets/Srcipts/UI/UIController.cs
@@ -9,23 +9,6 @@ public class UIController : MonoBehaviour
 {
     public static UIController instance;
 
-    public GameObject btnBuyTower;
-    public GameObject btnUpgradeAndSellTower;
-    public GameObject btnUpgradeTower;
-    public GameObject btnSellTower;
-    [SerializeField]
-    int towerPlacementIndex;
-    GameObject currentTower;
-
-    public Text txtWave;
-    public Text txtHealth;
-    public Text txtWood;
-
-    public GameObject btnBuyCanonTower;
-    public GameObject btnBuyArcherTower;
-    public GameObject btnBuyMagicTower;
-    public GameObject btnBuyLightningTower;
-
     private void Awake()
     {
         if (instance == null)
@@ -33,49 +16,6 @@ public class UIController : MonoBehaviour
             instance = this;
         }
     }
-
-    public void OpenBtnBuyTower(Transform targetPosition, int placementIndex)
-    {
-     
-  
-        btnBuyTower.transform.DOKill();
-        btnBuyTower.SetActive(false);
-        btnBuyTower.transform.position = new Vector3(targetPosition.position.x, targetPosition.position.y, btnBuyTower.transform.position.z);
-        btnBuyTower.transform.localScale = new Vector3(0, 0, 0);
-        btnBuyTower.SetActive(true);
-        btnBuyTower.transform.DOScale(1, 0.5f).SetEase(Ease.OutBack);
-        towerPlacementIndex = placementIndex;
-    }
-
-    
-
-    public void CloseBtnBuyTower()
-    {
-        btnBuyTower.transform.DOKill();
-        btnBuyTower.SetActive(false);
-    }
-
-    public void ButtonBuyTower(int index)
-    {
-        AudioController.instance.PlaySound("buyTower");
-        CloseBtnBuyTower();
-        switch (index)
-        {
-            case 1:
-              
-                break;
-            case 2:
-               
-                break;
-            case 3:
-           
-                break;
-            case 4:
-                
-                break;
-        }
-    }
-
 
     [Header("Panel Home")]
     public GameObject pnlHome;
